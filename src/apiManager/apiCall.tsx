4dfc838b417f7a230bad4570b5135d255c9123ae
Muhammad Nabeel Mehmood <nabeel@rebeltechnology.io>
2022-03-22 14:34:21 +0500

    Implemented Api Manager.

---
+++ src/apiManager/apiCall.tsx
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+const apiCall = (method:string, url:string, data:string)=>{
+    return new Promise((resolve,reject)=>{
+        axios[method](
+            url,
+            data
+        ).then((response:any)=>{
+            resolve(response);
+        }).catch((error:any)=>{
+            reject(error)
+            throwError(error);
+        })
+    });
+}
+
+const throwError = (error:any)=>{
+    new Error("Something went wrong");
+}
+
+export default apiCall;
+
+
