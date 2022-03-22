1abd3a7b9933cc64c46567619d4be01212d09fd2
Muhammad Nabeel Mehmood <nabeel@rebeltechnology.io>
2022-03-22 14:19:46 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import './App.css';
+function App() {
+  return (
+    <div>
+        <header>
+            <h1>Test App</h1>
+        </header>
+        <main>
+            <button type="button">Go</button>
+        </main>
+    </div>
+
+  );
+}
+
+export default App;
